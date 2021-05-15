7bcf4f30b4e94ea731b8dbb53b45fb03259623be
MauroBraga <M@r202150016774>
2021-05-15 18:33:53 -0300

    Criando categoria

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
-import express, { response } from 'express';
+import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json())
+app.use(categoriesRoutes);
 
-app.get("/", (req, resp) =>{
-     return resp.json({message: "Hello World"});
-})
-
-app.post("/courses", (req,res) =>{
-     const { name } = req.body;
-     return res.json({name})
-})
 
 app.listen(3333, () => console.log('Server is running'));
